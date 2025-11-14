556546f343105db5e7c5e612ca661e07ca34f5b0
Viniffx <viniffx@gmail.com>
2025-11-14 11:10:49 -0300

    carrossel ok, da pra melhorar

--- js/carrousel.js
+++ js/carrousel.js
@@ -11,6 +11,7 @@ let currentIndex = 0; // contador para lista
 function updateCaroussel() {
     carousselContent.classList.remove(...listaLivros); //... spread sintax, descompõe o array em elementos individuais
     carousselContent.classList.add(listaLivros[currentIndex]);
+
 }
 
 nextBtn.addEventListener("click", () => {
@@ -35,41 +36,3 @@ prevBtn.addEventListener("click", () => {
 updateCaroussel();
 
 
-
-
-const API = "http://localhost:3000/carrousel"
-
-const cliqueGuardado = localStorage.getItem('botaoClicado')
-
-async function DadosDoBanco() {
-    try {
-        const response = await fetch(API);
-        if (!response.ok) {
-            throw new Error('Erro na requisição à API');
-        }
-
-        const dados = await response.json();
-        console.log('Dados recebidos:', dados);
-        return dados; // retorna os dados para serem usados depois
-
-    } catch (error) {
-        console.error('Erro ao buscar dados:', error);
-        return null;
-    }
-}
-
-if (cliqueGuardado === "sim") {
-    document.getElementById("mensagem").innerText = "O botão na página anterior foi clicado";
-}
-
-const livroSelecionado = JSON.parse(localStorage.getItem("livroSelecionado"));
-
-if (livroSelecionado) {
-    document.getElementById("titulo").innerText = livroSelecionado.titulo;
-    document.getElementById("autor").innerText = livroSelecionado.autor;
-    document.getElementById("capa").src = livroSelecionado.capa_url;
-
-
-}
-
-localStorage.removeItem('botaoClicado');

5f2ea8d6f6f24b206055456a23dfd5cd824d59d7
nikollas-dh <nikollasdheyvis17@gmail.com>
2025-12-25 13:01:27 -0300

    Estilizando tela login

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ import mysql from "mysql2/promise";
 export const db = await mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "1234",
+  password: "senai",
   database: "bd_bibliotec",
   port: 3306,
 });

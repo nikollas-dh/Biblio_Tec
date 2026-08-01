aa5af9f0815e3fe2232fba51942caff50902b8d0
nikollas-dh <nikollasdheyvis17@gmail.com>
2026-08-01 19:29:42 -0300

    limpando projeto e arrumando responsividade

--- src/routes/curso.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { listarCursos, obterCursos } from "../controllers/cursoController.js";
-
-const router = express.Router();
-router.get("/", listarCursos);
-router.get("/:id", obterCursos);
-
-export default router;

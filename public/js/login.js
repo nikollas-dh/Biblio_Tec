0567d945c0f0ee80276b4d0e48b60b9968e803e7
nikollas-dh <nikollasdheyvis17@gmail.com>
2026-08-04 15:49:35 -0300

    fazendo a primeira integração do aspnet com o front

--- public/js/login.js
+++ public/js/login.js
@@ -1,51 +1,59 @@
-const API = "http://localhost:3000/api/usuario/login"
+const API = "https://localhost:7119/api/auth/login";
 
-
-const bntLogin = document.getElementById('bntLogin');
 const inputEmail = document.getElementById("email");
 const inputSenha = document.getElementById("senha");
-const formLogin = document.getElementById("form-login")
-
+const formLogin = document.getElementById("form-login");
 
 async function carregandoUsuario(e) {
-  e.preventDefault();
-  console.log("Procurando Usuario...");
-  const email = inputEmail.value.trim();
-  const senha = inputSenha.value.trim();
+    e.preventDefault();
 
+    const email = inputEmail.value.trim();
+    const senha = inputSenha.value.trim();
 
-
-  if (!email || !senha) {
-    alert("Gentileza preecher os campos email e senha!")
-    return;
-  }
-
-  const Usuario = {
-    email, senha
-  }
-  try {
-    const requisicao = await fetch(API, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(Usuario)
-    })
-    if (requisicao.status === 200) {
-      const dados = await requisicao.json();
-      console.log(dados)
-      const perfil = dados.perfil
-
-      alert("Entrando...")
-      if (dados.perfil === "Admin") {
-        window.location.href = "menu_admin.html"
-      } else {
-        window.location.href = "principal.html"
-      }
-    } else {
-      console.log("Email ou senha inválidos")
-      alert("Email ou senha inválidos")
+    if (!email || !senha) {
+        alert("Gentileza preencher os campos email e senha!");
+        return;
     }
-  } catch (error) {
-    console.error(error)
-  }
+
+    const usuario = {
+        email,
+        senha
+    };
+
+    try {
+        const requisicao = await fetch(API, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(usuario)
+        });
+
+        if (!requisicao.ok) {
+            alert("Email ou senha inválidos!");
+            return;
+        }
+
+        const dados = await requisicao.json();
+        
+        console.log(dados);
+        localStorage.setItem("token", dados.token);
+        if (dados.perfil) {
+            localStorage.setItem("perfil", dados.perfil);
+        }
+
+        alert("Entrando...");
+
+        if (dados.perfil === "Administrador") {
+            window.location.href = "menu_admin.html";
+        } else {
+            window.location.href = "principal.html";
+        }
+
+    } catch (erro) {
+        console.error(erro);
+        alert("Erro ao conectar com a API.");
+    }
 }
-formLogin.addEventListener("submit", carregandoUsuario)
+
+formLogin.addEventListener("submit", carregandoUsuario);
